5c701ded4e1ebbcb28cd8444c89916f901c85278
seumbae <ohcm0013@gmail.com>
2023-12-06 18:53:50 -0600

    chore

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => { 
-    res.send('Hello Jenkins!')
+    res.send('Happy Hacking!')
 });
 
 const port = process.env.PORT || 5000;
